3a56e33917234796595d9c7855c21667c01ef309
InesBarone <minesbarone@gmail.com>
2022-10-12 21:03:36 -0300

    hola

--- front/src/Pages/Formulario/formulario.js
+++ front/src/Pages/Formulario/formulario.js
@@ -3,6 +3,7 @@ import "./formulario.css";
 
 export default function Formulario() {
   const [newName, setnewName] = useState("");
+  const [newImg, setnewImg] = useState("");
   const [newWeight, setnewWeight] = useState("");
   const [newHeight, setnewHeight] = useState("");
   const [newDescription, setnewDescription] = useState("");
@@ -28,61 +29,106 @@ export default function Formulario() {
     <div>
       <form action="" className="formulario">
         <h1 className="formulario__titulo">Agregar Pokemon</h1>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewName(e.target.value)}
+        />
         <label for="" className="formulario__label">
           Name
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewImg(e.target.value)}
+        />
         <label for="" className="formulario__label">
           Pokemon Image
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewWeight(e.target.value)}
+        />
         <label for="" className="formulario__label">
           Weight
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewHeight(e.target.value)}
+        />
         <label for="" className="formulario__label">
           Height
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewDescription(e.target.value)}
+        />
         <label for="" className="formulario__label">
           Description
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewHP(e.target.value)}
+        />
         <label for="" className="formulario__label">
           HP
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewATK(e.target.value)}
+        />
         <label for="" className="formulario__label">
           ATK
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewDEF(e.target.value)}
+        />
         <label for="" className="formulario__label">
           DEF
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewSATK(e.target.value)}
+        />
         <label for="" className="formulario__label">
           SATK
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewSDEF(e.target.value)}
+        />
         <label for="" className="formulario__label">
           SDEF
         </label>
-        <input type="text" className="formulario__input" />
+        <input
+          type="text"
+          className="formulario__input"
+          onChange={(e) => setnewSPD(e.target.value)}
+        />
         <label for="" className="formulario__label">
           SPD
         </label>
         <form className="type_form">
           Pokemon type
-          <select>
+          <select onChange={(e) => setnewType(e.target.value)}>
             <option disabled>Select Pokemon type</option>
-            <option>Poison</option>
-            <option>Electric</option>
-            <option>Fire</option>
-            <option>Water</option>
-            <option>Flying</option>
-            <option>Ghost</option>
+            <option value="1">Poison</option>
+            <option value="2">Electric</option>
+            <option value="3">Fire</option>
+            <option value="4">Water</option>
+            <option value="5">Flying</option>
+            <option value="6">Ghost</option>
           </select>
+          {console.log(newType)}
         </form>
         <input
           type="button"
