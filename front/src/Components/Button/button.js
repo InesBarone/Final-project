1b28db6fd27dbf499cdd0a2173b23f7a555d1715
InesBarone <minesbarone@gmail.com>
2022-10-18 20:36:23 -0300

    share

--- front/src/Components/Button/button.js
+++ front/src/Components/Button/button.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./button.css";
 
-export default function Button({text}){
-    return (
-        <div className="button-green"> 
-        <button>{text}</button>
-        </div>
-    )
-} 
+export default function Button({ text, handleSharePokemon }) {
+  return (
+    <div className="button-green">
+      <button
+        onClick={(e) => {
+          handleSharePokemon();
+        }}
+      >
+        {text}
+      </button>
+    </div>
+  );
+}
